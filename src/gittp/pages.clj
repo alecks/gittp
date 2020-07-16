b8afd432d2ca05df4e9eca811335a37869e001bf
Alex P <alexeek@protonmail.com>
2020-07-16 16:23:07 +0100

    feat: add home/not-found pages

--- src/gittp/pages.clj
+++ src/gittp/pages.clj
@@ -1,7 +1,29 @@
-(ns gittp.pages)
+(ns gittp.pages
+  (:require [hiccup.core :refer :all]))
+
+(def site-title "gittp")
+
+(defn- full-title [title]
+  (str title " — " site-title))
+
+(defn- html-wrapper [title & content]
+  (html
+    [:html
+     [:head
+      [:title (full-title title)]]
+     [:body
+      content]]))
 
 (defn home []
-  "home")
+  (let [title "home"]
+    (html-wrapper
+      title
+      [:h1 title]
+      [:p "TODO"])))                                        ; TODO(fjah): add content
 
 (defn not-found []
-  "not found")
+  (let [title "not found"]
+    (html-wrapper
+      title
+      [:h1 title]
+      [:p "404 — this page couldn't be found."])))
