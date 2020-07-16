9675f3608465a250c133001ec8b5e1cc18abedee
Alex P <alexeek@protonmail.com>
2020-07-16 16:39:08 +0100

    chore(deps): add jgit

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [clj-jgit "1.0.0-beta3"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler gittp.handler/app}
   :profiles
