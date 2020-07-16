70a19ea5bbdc51abb61c2c922066645083116377
Alex P <alexeek@protonmail.com>
2020-07-16 16:13:18 +0100

    feat: add pages namespace

--- src/gittp/handler.clj
+++ src/gittp/handler.clj
@@ -1,11 +1,12 @@
 (ns gittp.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [gittp.pages :as pages]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+           (GET "/" [] (pages/home))
+           (route/not-found (pages/not-found)))
 
 (def app
   (wrap-defaults app-routes site-defaults))
---
+++ src/gittp/pages.clj
@@ -0,0 +1,7 @@
+(ns gittp.pages)
+
+(defn home []
+  "home")
+
+(defn not-found []
+  "not found")
